3ee5084bac76fabde7b4a1d2eda14aa274692ca6
Sacha Durand Saint Omer <sachadso@gmail.com>
2016-04-06 20:21:14 +0200

    Add Package.swift

---
+++ Package.swift
@@ -0,0 +1,10 @@
+import PackageDescription
+
+let package = Package(
+    name: "ws",
+    dependencies: [
+        .Package(url: "https://github.com/s4cha/Arrow",
+        .Package(url: "https://github.com/s4cha/then",
+        .Package(url: "https://github.com/Alamofire/Alamofire"
+    ]
+)
