f39f86ca2a14af33153ae1e12aedf88d42b163b9
f2m2rd <rocky@f2m2.com>
2017-09-22 16:04:49 -0700

    Update WSNetworkIndicator.swift
    Updated method 'tick()' to expose it to Objective-C.

--- ws/WSNetworkIndicator.swift
+++ ws/WSNetworkIndicator.swift
@@ -34,7 +34,7 @@ class WSNetworkIndicator: NSObject {
         RunLoop.main.add(timer, forMode: RunLoopMode.commonModes)
     }
     
-    func tick() {
+    @objc func tick() {
         let previousValue = UIApplication.shared.isNetworkActivityIndicatorVisible
         let newValue = (runningRequests != 0)
         if newValue != previousValue {
