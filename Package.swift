4de55cbdb5ca60440bb972d4f97e7a1f24bde1fa
S4cha <sachadso@gmail.com>
2019-12-23 18:05:53 +0100

    Updates fro SPM

--- Package.swift
+++ Package.swift
@@ -1,10 +1,18 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
 let package = Package(
     name: "ws",
+    platforms: [.iOS(.v8)],
+    products: [.library(name: "ws", targets: ["ws"])],
     dependencies: [
-        .Package(url: "https://github.com/freshOS/Arrow",
-        .Package(url: "https://github.com/freshOS/then",
-        .Package(url: "https://github.com/Alamofire/Alamofire"
+        .package(url: "https://github.com/freshOS/Arrow", .exact("5.1.2")),
+        .package(url: "https://github.com/freshOS/Then", .exact("5.1.3")),
+        .package(url: "https://github.com/Alamofire/Alamofire", .exact("4.9.1"))
+    ],
+    targets: [
+        .target(name: "ws", dependencies:["Arrow", "Then", "Alamofire"]),
+        .testTarget(name: "wsTests", dependencies: ["ws"])
     ]
 )
--- Sources/ws/WSNetworkIndicator.swift
+++ Sources/ws/WSNetworkIndicator.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import UIKit
 
 /**
     Abstracts network activity indicator management.
